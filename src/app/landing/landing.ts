2ef813419e9b508fcc591eff828275f0e8f75c85
yeferchofas2005-dev <yeferchofas2005@gmail.com>
2026-02-03 13:40:16 -0500

    Se agrega parte de landing page.

--- src/app/landing/landing.ts
+++ src/app/landing/landing.ts
@@ -1,15 +1,52 @@
-import { Component} from '@angular/core';
+import { Component } from '@angular/core';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-landing',
-  imports: [],
+  imports: [NgIf],
   templateUrl: './landing.html',
   styleUrl: './landing.css',
 })
-export class Landing{
-  menuOpen = false;
+export class Landing {
 
-  toggleMenu() {
-    this.menuOpen = !this.menuOpen;
+  /* ======================
+     POPUP / SLIDER
+  ====================== */
+  showIntroPopup = true;
+
+  slides = [
+    {
+      title: 'Bienvenido a Preicfes Gratuito',
+      text: 'Educación gratuita para jóvenes que sueñan con un mejor futuro.'
+    },
+    {
+      title: 'Inscripciones abiertas',
+      text: 'Únete a nuestro programa y prepárate para las pruebas Saber 11.'
+    },
+    {
+      title: 'Síguenos en Instagram',
+      text: 'Mantente informado sobre clases, eventos y noticias.'
+    },
+    {
+    title: 'Inscripciones abiertas',
+    text: 'Prepárate con nosotros',
+    img: 'assets/logo.png'
+    }
+  ];
+
+  currentSlide = 0;
+
+  nextSlide() {
+    this.currentSlide =
+      (this.currentSlide + 1) % this.slides.length;
+  }
+
+  prevSlide() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.slides.length) % this.slides.length;
+  }
+
+  closeIntroPopup() {
+    this.showIntroPopup = false;
   }
 }
