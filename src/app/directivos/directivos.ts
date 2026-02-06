00da6c63a2e5fbcdb1f0baa44302a91309cc0dc6
yeferchofas2005-dev <yeferchofas2005@gmail.com>
2026-02-05 23:59:47 -0500

    Se agrega parte landing page

--- src/app/directivos/directivos.ts
+++ src/app/directivos/directivos.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-directivos',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './directivos.html',
   styleUrl: './directivos.css',
 })
